33670d46a0bad485b202fcd1fbedd0bdb405f6ae
Kris Rybak <kris.rybak@hotmail.co.uk>
2021-03-29 16:18:30 +0100

    Adds subquery option

--- data/filter.go
+++ data/filter.go
@@ -5,10 +5,23 @@ type Filter struct {
 	Logic      string
 	Filters    map[string]*Filter
 	Collation  bool
+	Subquery   *Subquery
+}
+
+type Subquery struct {
+	IsEnabled bool
+	Key       string
+	Set       string
 }
 
 func NewFilter() *Filter {
-	filter := &Filter{Logic: "and", Collation: true}
+	filter := &Filter{Logic: "and", Collation: true, Subquery: NewSubquery()}
 
 	return filter
+}
+
+func NewSubquery() *Subquery {
+	subquery := &Subquery{IsEnabled: false}
+
+	return subquery
 }

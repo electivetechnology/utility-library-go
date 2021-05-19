1491d66dc3cebb0c8e6cd4531c71fe6881249543
sammyH <sammyh@recii.io>
2021-05-19 17:11:03 +0100

    refactor test

--- hash/hash_test.go
+++ hash/hash_test.go
@@ -1,16 +1,15 @@
 package hash
 
 import (
-	"fmt"
 	"testing"
 	"unicode/utf8"
 )
 
 func TestGenerateHash(t *testing.T) {
 	length := 10
-	randomString := GenerateHash(length)
+	generatedString := GenerateHash(length)
 
-	if utf8.RuneCountInString(randomString) != length {
+	if utf8.RuneCountInString(generatedString) != length {
 		t.Errorf("Length of hash does not equal to %d", length)
 	}
 }
@@ -22,21 +21,16 @@ func TestGenerateMultipleHashes(t *testing.T) {
 	hashArray := []string{}
 
 	for i := 0; i < limit; i++ {
-		randomString := GenerateHash(length)
-		hashArray = append(hashArray, randomString)
-		fmt.Println("Generated hash", randomString)
+		generatedString := GenerateHash(length)
+		hashArray = append(hashArray, generatedString)
 	}
-
-	fmt.Println("hashArray", hashArray)
 
 	for i := 0; i < limit; i++ {
 		j := 0
 
 		for ; j < i; j++ {
 			if hashArray[j] == hashArray[i] {
-				fmt.Println("hashArray[j]", hashArray[j])
-				fmt.Println("hashArray[i]", hashArray[i])
-				t.Errorf("%s\n is a duplicate %s\n", hashArray[j], hashArray[i])
+				t.Errorf("%s\n is a duplicate of %s\n", hashArray[j], hashArray[i])
 			}
 		}
 

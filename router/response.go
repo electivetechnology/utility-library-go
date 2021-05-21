6ea95ee43e90074bbfdb2dc54d982296312ea653
Kris Rybak <kris.rybak@hotmail.co.uk>
2021-05-21 11:01:05 +0100

    Adds NotFound struct

--- router/response.go
+++ router/response.go
@@ -6,6 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type ResourceNotFound struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+}
+
 func NoContent(c *gin.Context) {
 	c.Status(http.StatusNoContent)
 }

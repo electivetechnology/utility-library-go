7bdac7eefd49fa58fd618b98687c2bb409c3d621
Kris Rybak <kris.rybak@hotmail.co.uk>
2021-06-07 18:47:45 +0100

    Adds google oauth client

---
+++ clients/google/token.go
@@ -0,0 +1,36 @@
+package google
+
+import "time"
+
+type Token struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	ExpiresIn    int    `json:"expires_in"`
+	ExpiresAt    *time.Time
+	RefreshToken string `json:"refresh_token"`
+	Scope        string `json:"scope"`
+}
+
+func (t Token) GetAccessToken() string {
+	return t.AccessToken
+}
+
+func (t Token) GetTokenType() string {
+	return t.TokenType
+}
+
+func (t Token) GetExpiresIn() int {
+	return t.ExpiresIn
+}
+
+func (t Token) GetRefreshToken() string {
+	return t.RefreshToken
+}
+
+func (t Token) GetExpiresAt() *time.Time {
+	return t.ExpiresAt
+}
+
+func (t Token) GetScope() string {
+	return t.Scope
+}

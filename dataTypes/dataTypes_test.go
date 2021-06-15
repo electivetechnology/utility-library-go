18b0a43b2c8b2ad955bccf4de3f300923ea00c11
Chris Dixon <chris.js.dixon@gmail.com>
2021-06-15 12:57:38 +0100

    client

--- dataTypes/dataTypes_test.go
+++ dataTypes/dataTypes_test.go
@@ -22,6 +22,70 @@ func formatResponse (fieldMapJSON string, dataJSON string) ElectiveResponse {
 	return ToElectiveStruct(fieldMap, data)
 }
 
+func TestToElectiveClientStruct(t *testing.T) {
+	fieldMapJSON := `{"client":{
+"Name":{"type":"string","field":"Name","DisplayName":"Name"},
+"Overview":{"type":"string","field":"Overview","DisplayName":"Overview"},
+"Linkedin":{"type":"string","field":"Linkedin","DisplayName":"Linkedin"},
+"Website":{"type":"string","field":"Website","DisplayName":"Website"},
+"Introduction":{"type":"string","field":"Introduction","DisplayName":"Introduction"},
+"PrimaryContactFirstName":{"type":"string","field":"PrimaryContactFirstName","DisplayName":"PrimaryContactFirstName"},
+"PrimaryContactLastName":{"type":"string","field":"PrimaryContactLastName","DisplayName":"PrimaryContactLastName"},
+"PrimaryContactEmail":{"type":"string","field":"PrimaryContactEmail","DisplayName":"PrimaryContactEmail"},
+"PrimaryContactPhone":{"type":"string","field":"PrimaryContactPhone","DisplayName":"PrimaryContactPhone"},
+"Status":{"type":"string","field":"Status","DisplayName":"Status"},
+"BrandColour":{"type":"string","field":"BrandColour","DisplayName":"BrandColour"}
+}}`
+
+
+	name 					:= "Coca cola"
+	overview 				:= "overview example"
+	linkedin 				:= "linkedin"
+	website 				:= "website"
+	introduction 			:= "introduction"
+	primaryContactFirstName	:= "chris"
+	primaryContactLastName 	:= "dixon"
+	primaryContactEmail 	:= "dixon@awesome.co.uk"
+	primaryContactPhone 	:= "+44231412423123"
+	status 					:= "active"
+	brandColour 			:= "red"
+
+	dataJSON := `{
+"Name":"` + name + `",
+"Overview":"` + overview + `",
+"Linkedin":"` + linkedin + `",
+"Website":"` + website + `",
+"Introduction":"` + introduction + `",
+"PrimaryContactFirstName":"` + primaryContactFirstName + `",
+"PrimaryContactLastName":"` + primaryContactLastName + `",
+"PrimaryContactEmail":"` + primaryContactEmail + `",
+"PrimaryContactPhone":"` + primaryContactPhone + `",
+"Status":"` + status + `",
+"BrandColour":"` + brandColour + `"
+}`
+
+	ret := formatResponse(fieldMapJSON, dataJSON)
+
+	if ret.error != "" {
+		t.Errorf(ret.error)
+	}
+
+	rep 						:= ClientResponse{}
+	rep.Name 					= name
+	rep.Overview 				= overview
+	rep.Linkedin 				= linkedin
+	rep.Website 				= website
+	rep.Introduction 			= introduction
+	rep.PrimaryContactFirstName = primaryContactFirstName
+	rep.PrimaryContactLastName 	= primaryContactLastName
+	rep.PrimaryContactEmail 	= primaryContactEmail
+	rep.PrimaryContactPhone 	= primaryContactPhone
+	rep.Status 					= status
+	rep.BrandColour 			= brandColour
+
+	assert.Equal(t, rep, ret.TransformedData)
+}
+
 
 
 func TestToElectiveCandidateStruct(t *testing.T) {
@@ -44,7 +108,7 @@ func TestToElectiveCandidateStruct(t *testing.T) {
 }}`
 
 
-	email := "dixon@awesome.co.uk"
+	email 					:= "dixon@awesome.co.uk"
 	firstName 				:= "chris"
 	lastName 				:= "dixon"
 	phone 					:= "+442314124124"
@@ -104,21 +168,21 @@ func TestToElectiveCandidateStruct(t *testing.T) {
 	}
 
 	rep 						:= CandidateResponse{}
-	rep.Email = email
-	rep.FirstName = firstName
-	rep.LastName = lastName
-	rep.Phone = phone
-	rep.PrimaryLanguage = primaryLanguage
-	rep.SecondaryLanguage = secondaryLanguage
-	rep.TertiaryLanguage = tertiaryLanguage
-	rep.CvText = cvText
-	rep.AlternativePhoneNumber = alternativePhoneNumber
-	rep.Dob = dob
-	rep.VendorId = vendorId
-	rep.VendorStatus = vendorStatus
-	rep.VendorSource = vendorSource
-	rep.Gender = gender
-	rep.Status = status
+	rep.Email 					= email
+	rep.FirstName 				= firstName
+	rep.LastName 				= lastName
+	rep.Phone 					= phone
+	rep.PrimaryLanguage 		= primaryLanguage
+	rep.SecondaryLanguage 		= secondaryLanguage
+	rep.TertiaryLanguage 		= tertiaryLanguage
+	rep.CvText 					= cvText
+	rep.AlternativePhoneNumber 	= alternativePhoneNumber
+	rep.Dob 					= dob
+	rep.VendorId 				= vendorId
+	rep.VendorStatus 			= vendorStatus
+	rep.VendorSource 			= vendorSource
+	rep.Gender 					= gender
+	rep.Status 					= status
 
 
 	assert.Equal(t, rep, ret.TransformedData)

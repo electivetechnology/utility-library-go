9c3358bec6d9a704987679b0a5a8456d62914b71
Chris Dixon <chris.js.dixon@gmail.com>
2021-07-05 15:45:23 +0100

    code

--- sanitise/phone.go
+++ sanitise/phone.go
@@ -1,12 +1,23 @@
 package sanitise
 
 import (
+	"fmt"
 	"log"
 	"regexp"
 	"strings"
 )
 
-func Phone (input string) string {
+//type Regex struct {
+//	Pattern regexp.Regexp
+//}
+type Regex struct {
+	Pattern string
+}
+type Code struct {
+	Country Regex
+}
+
+func Phone(input string) string {
 	output := strings.TrimSpace(input)
 
 	splitString := strings.FieldsFunc(output, SplitBySlash)
@@ -21,6 +32,29 @@ func Phone (input string) string {
 	output = reg.ReplaceAllString(output, "")
 
 	output = strings.TrimLeftFunc(output, TrimByZero)
+
+	return output
+}
+
+func PhoneCode(input string) string {
+	output := input
+	var codes map[int]string
+
+	codes[998] = "/^\\971[1-7,9][0-9]{7,8}$/"
+	codes[998] = "/^\\971[1-7,9][0-9]{7,8}$/"
+
+	for code, regex := range codes {
+		reg, err := regexp.Compile(regex)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		find := reg.FindString(input)
+		fmt.Println(code)
+		fmt.Println(find)
+		//if reg.FindString(input){}
+	}
 
 	return output
 }

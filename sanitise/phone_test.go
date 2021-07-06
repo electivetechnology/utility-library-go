3e80734fa4b9299ea79253ffb63ca2486d94ee25
Chris Dixon <chris.js.dixon@gmail.com>
2021-07-06 12:20:35 +0100

    test

--- sanitise/phone_test.go
+++ sanitise/phone_test.go
@@ -6,11 +6,20 @@ import (
 )
 
 var phone = "7810362111"
+var country = "44"
 
 func TestToSanitise(t *testing.T) {
-	input	:= "0000( " +phone + "+) adasdasd/  8273192837"
+	input := "0000( " + phone + "+) adasdasd/  8273192837"
 
-	ret := Phone(input)
+	ret := Phone(input, "")
 
 	assert.Equal(t, phone, ret)
+}
+
+func TestToSanitiseWithCountry(t *testing.T) {
+	input := "0000( " + phone + "+) adasdasd/  8273192837"
+
+	ret := Phone(input, country)
+
+	assert.Equal(t, country+phone, ret)
 }

2b48f359c62bc4a4991ceb5eb0fad0c6aa7a4a99
Kris Rybak <kris.rybak@hotmail.co.uk>
2022-01-13 10:54:23 +0000

    Adds messaging contracts

--- clients/messaging/contracts/message.go
+++ clients/messaging/contracts/message.go
@@ -9,3 +9,31 @@ type MessageInterface interface {
 	GetContent() string
 	GetSendAfter() time.Time
 }
+
+type Message struct {
+	To        string    `json:"to"`
+	From      string    `json:"from"`
+	Content   string    `json:"content"`
+	Subject   string    `json:"subject"`
+	SendAfter time.Time `json:"send_after"`
+}
+
+func (message Message) GetTo() string {
+	return message.To
+}
+
+func (message Message) GetFrom() string {
+	return message.From
+}
+
+func (message Message) GetSubject() string {
+	return message.Subject
+}
+
+func (message Message) GetContent() string {
+	return message.Content
+}
+
+func (message Message) GetSendAfter() time.Time {
+	return message.SendAfter
+}

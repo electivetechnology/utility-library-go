29d70c2ed8f872b51f59800996c18af8c4c56232
Kris Rybak <kris.rybak@hotmail.co.uk>
2022-01-24 11:29:25 +0000

    Adds method to refresh VIncere token

--- clients/vincere/client.go
+++ clients/vincere/client.go
@@ -91,6 +91,7 @@ func (client *OAuthClient) GetToken(auth oauth.Authorization) (oauth.Token, erro
 	return &Token{}, errors.New("error exchanging Authorization for Access token")
 }
 
+// Refresh is not implemented by Vincere
 func (client *OAuthClient) Refresh(refreshToken string) (oauth.Token, error) {
 	// If we got here there was some kind of error with exchange
 
@@ -98,6 +99,54 @@ func (client *OAuthClient) Refresh(refreshToken string) (oauth.Token, error) {
 }
 
 func (client *OAuthClient) RefreshToken(token oauth.Token, clientId string, clientSecret string) (oauth.Token, error) {
+	// Set URL parameters on declaration
+	values := url.Values{
+		"grant_type":    []string{oauth.GRANT_TYPE_REFRESH_TOKEN},
+		"refresh_token": []string{token.GetRefreshToken()},
+		"client_id":     []string{clientId},
+	}
+
+	log.Printf("Sending following data to vincere for refresh: %v", values)
+
+	// Perform Request
+	c := &http.Client{}
+	r, _ := http.NewRequest(http.MethodPost, client.BaseUrl+AUTH_TOKEN_URL, strings.NewReader(values.Encode())) // URL-encoded payload
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	r.Header.Add("Content-Length", strconv.Itoa(len(values.Encode())))
+
+	res, err := c.Do(r)
+
+	// Check for errors, default evaluation is false
+	if err != nil {
+		log.Printf("Error refreshing token: %v\n", err)
+		return &Token{}, errors.New("error exchanging Authorization for Access token")
+	}
+
+	// read all response body
+	data, _ := ioutil.ReadAll(res.Body)
+
+	// defer closing response body
+	defer res.Body.Close()
+
+	// print `data` as a string
+	log.Printf("%s", data)
+
+	// Success, populate token
+	if res.StatusCode == http.StatusOK {
+		tkn := Token{}
+		json.Unmarshal(data, &tkn)
+
+		// Set ExpiresAt
+		t := time.Now().Add(time.Second * time.Duration(tkn.ExpiresIn))
+		tkn.ExpiresAt = &t
+
+		// Add existing refresh token (vincare uses one refresh token)
+		tkn.RefreshToken = token.GetRefreshToken()
+
+		// Return token
+		return tkn, nil
+	}
+
 	// If we got here there was some kind of error with exchange
 
 	return Token{}, errors.New("error exchanging Token for Access token")

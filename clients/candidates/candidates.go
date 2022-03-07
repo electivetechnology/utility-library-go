d7c74665c03d946852cfd164c9d5c8d5692e93e9
Kris Rybak <kris.rybak@hotmail.co.uk>
2022-03-07 12:08:31 +0000

    Adds endpoint to get Candidate by ID

--- clients/candidates/candidates.go
+++ clients/candidates/candidates.go
@@ -121,6 +121,77 @@ func (client Client) GetCandidateByVendor(vendor string, vendorId string, token 
 	return response, errors.New("error getting candidate for given vendor")
 }
 
+func (client Client) GetCandidateById(id string, token string) (CandidateResponse, error) {
+	log.Printf("Will request candidate details for id %d", id)
+
+	// Generate new path replacer
+	r := strings.NewReplacer(":candidate", id)
+	path := r.Replace(GET_CANDIDATE_URL)
+	log.Printf("New path generated for request %s", path)
+
+	request, _ := http.NewRequest(http.MethodGet, client.ApiClient.GetBaseUrl()+path, nil)
+
+	// Set Headers for this request
+	request.Header.Set("Authorization", "Bearer "+token)
+	request.Header.Add("Content-Type", "application/json")
+
+	// Get key
+	key := client.ApiClient.GenerateKey(CANDIDATE_TAG_PREFIX + path + token)
+
+	// Perform Request
+	res, err := client.ApiClient.HandleRequest(request, key)
+
+	// Check for errors, default evaluation is false
+	if err != nil {
+		log.Printf("Error getting Candidate details: %v", err)
+		return CandidateResponse{}, connect.NewInternalError(err.Error())
+	}
+
+	// Success, populate token
+	response := CandidateResponse{ApiResponse: res}
+
+	// Check if the request was actually made
+	if !res.WasRequested {
+		// No request was made, let's return fake response
+		// This will be exactly the same token as requested for exchange
+		return CandidateResponse{}, nil
+	}
+
+	// read all response body
+	data := res.HttpResponse.Body
+
+	// print `data` as a string
+	log.Printf("%s\n", data)
+
+	switch res.HttpResponse.StatusCode {
+	case http.StatusOK:
+		candidate := Candidate{}
+		json.Unmarshal(data, &candidate)
+
+		// Check if respose was from Cache
+		if !res.WasCached {
+			// Save response to cache
+			log.Printf("Client provided fresh/uncached response. Saving response to cache with TTL %d", client.ApiClient.GetRedisTTL())
+
+			// Generate tags for cache
+			var tags []string
+			tags = append(tags, CANDIDATE_TAG_PREFIX+candidate.Id)
+			tags = append(tags, key)
+			client.ApiClient.SaveToCache(key, res, tags)
+		}
+
+		// Return response
+		response.Candidate = &candidate
+
+		return response, nil
+
+	default:
+		return response, nil
+	}
+
+	return response, errors.New("error getting candidate for given vendor")
+}
+
 func (client Client) PutCandidate(payload []byte, token string) (CandidateResponse, error) {
 	log.Printf("Sending request to Create Candidate")
 
